e9ea88806fd15b8c1e69b46716812b16d98bd3cc
Amit Vikram <amvi19cs@cmrit.ac.in>
2022-01-31 16:23:38 +0530

    chargesheetadded

--- admin/savevictim.php
+++ admin/savevictim.php
@@ -28,11 +28,11 @@ $output = array('error' => false);
 //  $username=$_POST['cid']; 
 $gender = $_POST['gender'];
 $age = $_POST['age'];
-$crid = $_POST['crid'];
+
 $vid = $_POST['vid'];
 
-$sql = "INSERT INTO Victim (Crid, FName, LName, Gender, Age, Victim_id)
-                                       VALUES('$crid','$fname','$lname','$gender','$age','$vid'); ";
+$sql = "INSERT INTO Victim ( FName, LName, Gender, Age, Victim_id)
+                                       VALUES('$fname','$lname','$gender','$age','$vid'); ";
 
                    $success = mysqli_query($dbcon,$sql);
 
